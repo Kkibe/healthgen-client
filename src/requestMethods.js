1bc9ddcd7826d5442a21f6ac5d963a7d2be5355e
Kibet Korir <88145946+K-kibet@users.noreply.github.com>
2023-02-10 10:29:46 +0300

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser ? JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken : ''
+const TOKEN = localStorage.getItem('persist:root') ? JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken : ''
 const BASE_URL = 'https://healthgen-api.onrender.com/api';
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
